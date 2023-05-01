56214ad02abd49cdc685e2ea532155fd60926753
Garrett Chaney <gbcpursuits@gmail.com>
2023-05-01 22:49:00 +1000

    We rewrote everything pertaining to our GraphQL Schemas, and life is good.

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,7 +1,8 @@
 import { GraphQLSchema } from 'graphql';
-import RootQuery from './Resolvers/RootQuery.js';
-import RootMutation from './Resolvers/RootMutation.js';
+import RootQuery from './queries/index.js';
+import RootMutation from './mutations/index.js';
 
+// Export the GraphQL schema for use in the server.
 export default new GraphQLSchema({
 	query: RootQuery,
 	mutation: RootMutation,
