7026f740e7352c3c26443858f4ca98782b4d72d9
Garrett Chaney <gbcpursuits@gmail.com>
2023-05-15 19:46:00 +1000

    heroku deploy

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
 		const conn = await mongoose.connect(process.env.MONGODB_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-			useCreateIndex: true,
 		});
 
 		console.log(
